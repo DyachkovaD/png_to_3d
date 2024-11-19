aeaa1b318d5d9097e65752e5e5f49a2d1fc0aab2
Darya Dyachkova <d.dyachkova@bk.ru>
2024-11-19 22:08:58 +0300

    добавила плашку к файлу .stl

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import segno
 import timeit
 
 
-def make_qr(url: str, scale=10, border=4) -> None:
+def make_qr(url: str, scale=10, border=0) -> None:
     """
     Делает QR-код в формате png из строки
     url - строка для кодирования
@@ -23,7 +23,7 @@ def make_qr(url: str, scale=10, border=4) -> None:
     print('QR-код создан')
 
 
-def png_qr_to_slt(png_file, depth) -> None:
+def png_qr_to_slt(png_file, depth, floor) -> None:
     """
     Преобразует QR-код в формате png
     в документ в формате slt с высотой QR-кода depth
@@ -41,6 +41,32 @@ def png_qr_to_slt(png_file, depth) -> None:
     # Итерируемся по пикселям изображения
     for x in range(width):
         for y in range(height):
+
+            # Создаем подложку
+            v1 = np.array([x, y, 0])
+            v2 = np.array([x + 1, y, 0])
+            v3 = np.array([x, y + 1, 0])
+            v4 = np.array([x + 1, y + 1, 0])
+            v5 = np.array([x, y, -floor])
+            v6 = np.array([x + 1, y, -floor])
+            v7 = np.array([x, y + 1, -floor])
+            v8 = np.array([x + 1, y + 1, -floor])
+            base_triangles = np.array([
+                [v1, v2, v3],
+                [v2, v4, v3],
+                [v5, v6, v7],
+                [v6, v8, v7],
+                [v1, v2, v6],
+                [v1, v6, v5],
+                [v1, v3, v7],
+                [v1, v7, v5],
+                [v2, v4, v8],
+                [v2, v8, v6],
+                [v3, v4, v8],
+                [v3, v8, v7],
+            ])
+            faces.extend(base_triangles)
+
             # Если пиксель тёмный, выдвигаем его на заданную высоту
             if img.getpixel((x, y)) < 128:
                 # Добавляем вершины для треугольников
@@ -88,6 +114,6 @@ def png_qr_to_slt(png_file, depth) -> None:
 make_qr('https://www.python.org/')
 img = Image.open('qr_code.png')
 
-exec_time = timeit.timeit(lambda: png_qr_to_slt(img, 10), number=1)
+exec_time = timeit.timeit(lambda: png_qr_to_slt(img, 10, 10), number=1)
 print(f"Время выполнения: {exec_time:.10f} секунд")
 
